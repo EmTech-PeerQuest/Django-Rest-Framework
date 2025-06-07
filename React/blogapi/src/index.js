bf81c57771d06b953b7669f6dfea99300b96578e
Jenel <jenelesteron01@gmail.com>
2025-06-07 14:28:17 +0800

    React Frame Done

--- React/blogapi/src/index.js
+++ React/blogapi/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const theme = createTheme();  // you can customize this if you want
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Optional: service worker (you can still use it if needed)
+//import * as serviceWorker from './serviceWorker';
+//serviceWorker.unregister();
+
