f873acf565e790521b70c403764f2878db0c1926
MuteX16 <mjwayneayabes@gmail.com>
2025-06-16 22:10:24 +0800

    Added JWT Generator and Register

--- DRF/users/serializers.py
+++ DRF/users/serializers.py
@@ -4,20 +4,20 @@ from users.models import NewUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     """
-    Currently unused in preference of the below.
+    Serializer for registering new users.
     """
     email = serializers.EmailField(required=True)
     user_name = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)  # ✅ Include this
     password = serializers.CharField(min_length=8, write_only=True)
 
     class Meta:
         model = NewUser
-        fields = ('email', 'user_name', 'password')
+        fields = ('email', 'user_name', 'first_name', 'password')  # ✅ Add 'first_name'
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        # as long as the fields are the same, we can just use this
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
