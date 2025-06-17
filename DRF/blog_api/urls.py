b1a42f2a58d9ae762f004259a8ebb435b7a44c09
Jenel <jenelesteron01@gmail.com>
2025-06-17 22:59:06 +0800

    Resolved merge conflict in db.sqlite3 by keeping local version

--- DRF/blog_api/urls.py
+++ DRF/blog_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, PostListDetailfilter, PostList, PostDetail, PostSearch
+from .views import PostViewSet, PostListDetailfilter, PostList, PostDetail, PostSearch, PostQueryRetrieveView
 
 app_name = 'blog_api'
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+router.register('posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('posts/', PostDetail.as_view(), name='detailcreate'),
@@ -13,4 +13,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('', PostList.as_view(), name='listcreate'),
     path('search/', PostListDetailfilter.as_view(), name='postsearch'),
+    path('posts/search/', PostListDetailfilter.as_view(), name='postsearch'),
+    path('posts/query/', PostQueryRetrieveView.as_view(), name='postquery'),  # 🔥 New route
 ]
