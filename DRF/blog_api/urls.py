eaed5a4a1850f920a8990b415794e4f72848b7ca
MuteX16 <mjwayneayabes@gmail.com>
2025-06-17 17:07:27 +0800

    Added Viewsets

--- DRF/blog_api/urls.py
+++ DRF/blog_api/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from .views import PostList, PostDetail, PostSearch, PostListDetailfilter
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, PostListDetailfilter
 
 app_name = 'blog_api'
 
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+
 urlpatterns = [
-    path('posts/', PostDetail.as_view(), name='detailcreate'),
-    #path('search/', PostSearch.as_view(), name='postsearch'),
+    path('', include(router.urls)),
     path('search/', PostListDetailfilter.as_view(), name='postsearch'),
-    path('', PostList.as_view(), name='listcreate'),
 ]
