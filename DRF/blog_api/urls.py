5b97d40a755167d9ef8cbedc3fdb0838803b859a
Jenel <jenelesteron01@gmail.com>
2025-06-17 22:50:02 +0800

    Resolved merge conflict in db.sqlite3 by keeping local version

--- DRF/blog_api/urls.py
+++ DRF/blog_api/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
-from .views import PostList, PostDetail, PostSearch, PostListDetailfilter
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, PostListDetailfilter, PostList, PostDetail, PostSearch
 
 app_name = 'blog_api'
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('posts/', PostDetail.as_view(), name='detailcreate'),
     #path('search/', PostSearch.as_view(), name='postsearch'),
-    path('search/', PostListDetailfilter.as_view(), name='postsearch'),
+    path('', include(router.urls)),
     path('', PostList.as_view(), name='listcreate'),
+    path('search/', PostListDetailfilter.as_view(), name='postsearch'),
 ]
--- DRF/blog_api/views.py
+++ DRF/blog_api/views.py
@@ -1,28 +1,47 @@
 from rest_framework import generics
+from rest_framework import viewsets, filters
 from blog.models import Post
 from .serializers import PostSerializer
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
 from rest_framework import filters
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import filters
 
+
+# Custom permission: only the author can edit/delete
 class PostUserWritePermission(BasePermission):
     message = 'Editing posts is restricted to the author only.'
 
     def has_object_permission(self, request, view, obj):
-
         if request.method in SAFE_METHODS:
             return True
-
         return obj.author == request.user
-
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = [AllowAny]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
+
+# ViewSet for Post model using routers
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [PostUserWritePermission()]
+        return [AllowAny()]
+
+
+# Optional: Search endpoint
+class PostListDetailfilter(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['$title']
 
 # IsAuthenticatedOrReadOnly
 
